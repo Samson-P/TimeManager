aacbb940eab20804c2d9ac97376b61ed15bdcd1a
Самсон <82338990+Samson-P@users.noreply.github.com>
2023-02-28 21:20:24 +0300

    реализовал ORM с помощью peewee

--- sqlite_adapter.py
+++ sqlite_adapter.py
@@ -1,23 +1,72 @@
 import sqlite3
+from peewee import *
 import datetime
+
 
 DB_NAME = 'dt_control.db'
 TABLE_NAME = 'dt_control'
+
+# Поля таблицы БД для
 TABLE_SCHEMA = ['id_dt_interval', 'title', 'description', 'dt_start', 'interval']
 
+# SQL для создания таблицы
 CREATE_TABLE_QUERY = f'''
-    CREATE TABLE {TABLE_NAME}
-        (id_dt_interval INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT NOT NULL,
-        description TEXT,
-        dt_start TEXT NOT NULL,
-        interval TEXT NOT NULL)
+CREATE TABLE dt_control (
+    id_dt_interval INTEGER PRIMARY KEY AUTOINCREMENT,
+    title          TEXT    NOT NULL,
+    description    TEXT,
+    dt_start       TEXT    NOT NULL,
+    interval       TEXT    NOT NULL
+);
 '''
 
+# SQL для записи новой строки
 TABLE_INSERT_QUERY = '''
                 INSERT INTO {}(title, description, dt_start, interval) 
                 VALUES ('{}', '{}', '{}', '{}');
             '''
+
+
+# Определяем базовую модель от которой будут наследоваться остальные
+class BaseModel(Model):
+    class Meta:
+        database = SqliteDatabase(DB_NAME)
+
+
+# Определяем модель исполнителя
+class TMInterval(BaseModel):
+    id_dt_interval = AutoField(column_name='id_dt_interval')
+    title = TextField(column_name='title')
+    description = TextField(column_name='description', null=True)
+    dt_start = TextField(column_name='dt_start')
+    interval = TextField(column_name='interval')
+
+    def __init__(self, title, description='N/A', dt_start=None):
+        """
+
+        :param title: Род деятельности
+        :param description: Детали
+        """
+        super().__init__(title=title, description=description)
+        self.dt_start = dt_start or datetime.datetime.now()
+
+    def stop(self):
+        # Здесь реализовано вычитание dt_start из .now()
+        # interval представляет из себя экземпляр класса timedelta
+        self.interval = datetime.datetime.now() - self.dt_start
+        # И запись dt_start в формате ДД-ММ-ГГГГ ЧЧ:ММ:СС
+        self.dt_start = self.dt_start.strftime("%d-%m-%Y %H:%M:%S")
+
+        return self.interval
+
+    def __repr__(self):
+        return self.title
+
+    def serialize(self):
+        return self
+
+    class Meta:
+        table_name = TABLE_NAME
 
 
 class DBManager:
@@ -61,84 +110,6 @@ class DBManager:
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.con.close()
-
-
-# Основной класс
-class TimeInterval:
-    def __init__(self, interval, title, description=None, id_dt_interval=None, dt_start=None):
-        """
-
-        :param interval: Продолжительность работы
-        :param title: Заголовок, собственно, род деятельности
-        :param description: Описание, чем конкретно занимался, подзадачи
-        """
-        self.id = id_dt_interval
-        self.dt_start = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
-        self.interval = interval
-
-        self.title = title
-        self.description = description
-
-    def __repr__(self):
-        return self
-
-    def __str__(self):
-        return f"<{str(self.title)[:10]}..., в теч.{self.interval}c>"
-
-    def write(self):
-        """
-
-        :return: None если Ок, если проблемы -- str
-        """
-
-        # проверить, таблица с таким именем существует, если нет - создать
-        # если таблицы не было, и создать ее не получилось, выходим с Ошибкой
-        with DBManager() as db:
-
-            # команда на вставку строки в таблицу
-            table_insert_query = f'''
-                    INSERT INTO {TABLE_NAME}(title, description, dt_start, interval) 
-                    VALUES('{self.title}', '{self.description}', '{self.dt_start}', '{self.interval}');
-                '''
-
-            db.cursor.execute(table_insert_query)
-
-            db.con.commit()
-            return None
-
-    def read(self, limit=10):
-        """
-
-        :param limit: Ограничение, количество строк, которое прочитаем из БД
-        :return: None если Ok, если проблема -- str
-        """
-
-        # проверить, таблица с таким именем существует, если нет - создать
-        # если создали - завершаем программу
-        with DBManager() as db:
-            if db.error == 'table created, no point in reading':
-                return f"Table {TABLE_NAME} created successfully. Table is empty!", None
-
-            # прочитаем последние 10 записей
-            read_table_query = f"SELECT * FROM {TABLE_NAME} ORDER BY id_dt_interval DESC LIMIT {limit}"
-
-            db.cursor.execute(read_table_query)
-            rows = db.cursor.fetchall()
-            query_set: list = []
-
-            for row in rows:
-                tm = TimeInterval(
-                    id_dt_interval=row[0],
-                    title=row[1],
-                    description=row[2],
-                    dt_start=row[3],
-                    interval=row[4]
-                    )
-                query_set.append(tm)
-            return None, query_set
-
-    def show_now(self):
-        return f"{self.dt_start}\t|{str(self.title)[:10]}...\t|{str(self.description)[:10]}...\t|{self.interval}\n"
 
 
 if __name__ == "__main__":
