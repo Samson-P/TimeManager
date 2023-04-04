53dd2b4cf0fdae1b43e8015f1b667ef37f85f211
Самсон <82338990+Samson-P@users.noreply.github.com>
2023-04-04 14:42:53 +0300

    переписал db_exists()
    Чтобы устранить дальнейшие повторения кода, оптимизировал функцию под разные условия

--- os_filemanager.py
+++ os_filemanager.py
@@ -2,16 +2,28 @@ import os
 
 
 # Проверяем наличие файла с расширением .db в корневом каталоге
-def db_exists():
+def db_exists(counts):
     # Читаем корневой каталог
     files = os.listdir('.')
 
-    for file_name in files:
-        # Если такой файл существует, возвращаем его название
-        if '.db' in file_name:
-            return file_name
-    # Если файла не существует, возвращаем
-    return None
+    if counts == 'one':
+        for file_name in files:
+            # Если такой файл существует, возвращаем его название
+            if '.db' in file_name:
+                return file_name
+        # Если файла не существует, возвращаем
+        return None
+    elif counts == 'all':
+        db_files = []
+        for file_name in files:
+            # Собираем в массив все файлы с таким расширением
+            if '.db' in file_name:
+                db_files.append(file_name)
+        if len(db_files):
+            return db_files
+        # Если не существует ни одного файла с таким расширением, возвращаем
+        return None
+
 
 
 # Проверка на существование всех файлов программы
