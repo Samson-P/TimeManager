c0f1723d644d0a83fe45bec30c2864fee411307f
Самсон <82338990+Samson-P@users.noreply.github.com>
2023-04-10 14:49:54 +0300

    add models ConfigManager & FirstUseModel
    Убираю лишнее.
    
    Открытие ссылки при помощи webbrowser теперь делает ConfigManager.
    
    Вся работа с файлом конфига в этом модуле теперь проходит через специализированную модель ConfigManager, configparser теперь тоже здесь не нужен.

--- first_use.py
+++ first_use.py
@@ -1,21 +1,11 @@
-import webbrowser
 import PySimpleGUI as SimpleGUI
-import configparser
+from conf_creator import ConfManager
 import base64
+from sqlite_adapter import FirstUseModel
 
 
-setup_steps = {
-    'Welcome': 1,
-    'Interface personalization': 2,
-    'Confirm the selected settings': 3,
-}
+config = ConfManager()
 
-
-config = configparser.ConfigParser()  # создаём объекта парсера
-config.read("cnf/ui_configuration.ini")  # читаем конфиг
-
-# Достает цветовую схему из конфигурационного файла
-# SimpleGUI.theme(config["UI"]["theme"])
 SimpleGUI.set_options(font=("Courier Mono", 12))
 
 source_url_font = ('Courier Mono', 14, 'underline')
@@ -27,6 +17,9 @@ def confirm():
 
 
 def personalization():
+
+    first_use_model = FirstUseModel()
+
     # Шаблон окна персонализации
     Ru_locale = b'iVBORw0KGgoAAAANSUhEUgAAADAAAAAWCAYAAACG9x+sAAAACXBIWXMAAA7DAAAOwwHHb6hkAAAAGXRFWHRTb2Z0d2FyZQB3d3cuaW5rc2NhcGUub3Jnm+48GgAAAu5JREFUWIW9l99LU2EYx7/ve7blpm6w3cTOYnVT7hwiBJMudHVR4KQUr7qOgYhehPbjf0hIBXG7OgXdBSEyyB90UxNyIIjhPBoNodzcjWexoSic7bxdOEPKzene7XP58vDh+/A+h/c8BCUIBoNOYjL1EUJ6CHCDEeIBY42l6rlCyD5hLMmATQARQ9dnFEXJnFr670F/f7+NCcKISRBeyJJkl3w+iKIIh90OAMjmckilUlhXVagbG8jn87Vt5ogcAUYbrdax8fHxg5INDAwMiHlg5qYst3UHAnA5nWWtmqZhbmEBa/F4DTKfyjdDEHqVqamfxwd/G3gyOHjlEhALdHW5Ozs6zmWNLi5idn4ejDGOWUuyk6f0zttQaBsABOBobATgU3cgcP284QHA6/XCbDbjRyLBOeupNAuM3fN3dr6LxWJ5CgCMkOeyLLdeJPwxd/1+yJLELWU5GNC6d3j4DADI0NCQyyBka+TpU7vL5apKnMlk8Hpiom4ftqHr16huGH2yJFUdHgCcTid8LS0cslWEnVgsvZQx9pDn1ddrjACAMPaIApBFt5ub1OPxcHOdBQEkCuCyvfhI8cDB0XUWDBApd2l93oJjDAogncvluBl5uipghwJQk6kUN+M2R1cFbFAAkXVV5WZUOboqIEINXZ9ZV9XcrqZVbdMyGWxsbnLIVRFZQ9cjVFGUjFEojM4vLFRt/Dg7W69XGGDslaIoGQoAjVbr2Fo8vhJdXLyw73M0Cp6jeAYrTTbbBFD8G43FYvlb7e1zW4nEY7PF0nzV6z2X7Us0Ch43WCE7eUrvhycnNaDYAACsLi/nbre1vf+eSPjT6bRbFEXYbLaypl1Nw4fpaXxdWqpx5iKMrRom04M3odCv46P/Vsrh4WHr/sHBCBWEl5LPZ5clCR5RhMPhAABks1kkT6yUhUKhHtGzjJDR5oaG8bIr5UmCwaCTWCy9xDB6QEgLAA+AplonLbIHIAnGNhmlETMwEw6Hf59W+AeEBxzSTJhqkQAAAABJRU5ErkJggg=='
     En_locale = b'iVBORw0KGgoAAAANSUhEUgAAADAAAAAWCAYAAACG9x+sAAAACXBIWXMAAA7DAAAOwwHHb6hkAAAAGXRFWHRTb2Z0d2FyZQB3d3cuaW5rc2NhcGUub3Jnm+48GgAAAr9JREFUWIXFl0tPE1EYhp9v5syAiCwgXgItiXEhlxXEhTvBtXKJW417NZoU9T9IYkE0Ju68bF1gf4CAKzcWXXDZqLEdMF7AWBBs50yPixY1Ci3SoT7LM9955/2Sc2a+V9iCSNw0OpY/iCV9GI4CEWDvVvUh8w3wgHkRk8hpZ9yLyfJmhfLnQvM9U1eT1TEM14CGXTaKY0Nv1KcnGtDWGHCgzgDwcU2YX7aZSNlMek7GDxj2AxX3YrK+ZQORuGlxLD2OcGy3jQOcbPW50p0lss+UrPNWhFvJWp6m1Cts1f/2krzbePazgSM3TTSv9HOgefcsF7AFLndnOduR+6d9j2ZdbidrFo2vjr8ekjSABYVjk1f6CVUwDzszD3CuI8fFrmxzXuknkbjZA8UG3Ky+CnSFa3NzTrb6OzK/wfnOHL2tustReghAWu6YJjfQb6jShX18erXsmS/HwqpwJlGfWcupw5Yb+INUwTwUvjaVmgdoqTeciPgNru33WyCnQvC2LXqiQchactpCpDM01TK0N+rQtDqaAgx0WBhzKDTVMuyvC0+r+MNrscKTrC75wlXKW8D7ar300/pfk0ulWosWMBuaahnmluzQtGY/2wjMWSImEZpqGSZS4TUwmbbJi0lYOe2MA5nQlEswkXZIZyo/Rt6K8GzB+aq1k7CKc/Zw5fbKo/MwNl1bsc7Ii1p0wA0vJssWgB+ouAjJipW3wdOU4tGsu+P992dcpjyVzAVqFIrDnBeTdfHVALAYjs3SjCVreLiDJh7MuNydrlkUXw1sBJv/Gmh6WzVXur4TbSg9H6Uzwmiylsm0eomtBjYNNBtE4maPY+sYcJ0qDHnKgp5ipGxvCjhYjJQf1oS5pUKknPKcrzpg2M+rkZKR8ncicdPo2n4/In3G0EYh1Nfvaje/WAU8EeYxJqEdZzx1Qb5sVvgDAJEFQLjoGwcAAAAASUVORK5CYII='
@@ -60,6 +53,53 @@ def personalization():
         [SimpleGUI.Button('Выйти', key='Cancel'),
          SimpleGUI.Button('Далее', key='Next'),
          SimpleGUI.Button('Пропустить с настройками по умолчанию', key='Skip')],
+    advanced_settings_layout = [
+        [
+            SimpleGUI.Frame('UI:', [
+                [SimpleGUI.Text('Имя пользователя:')],
+            ],
+                         border_width=0),
+
+            SimpleGUI.Frame('Данные о пользователе', [
+                [SimpleGUI.InputText(default_text=first_use_model.uname, key='uname')],
+            ],
+                            border_width=1),
+        ],
+
+        [
+            SimpleGUI.Frame('DataBase:', [
+                [SimpleGUI.Text('Название файла базы данных:')],
+                [SimpleGUI.Text('Название таблицы базы данных:')],
+                [SimpleGUI.Text('Поля таблицы:')]
+            ],
+                            border_width=0),
+
+            SimpleGUI.Frame('Описание конфигурации БД', [
+                [SimpleGUI.InputText(
+                    default_text=first_use_model.database_db_name, key='db_name')],
+                [SimpleGUI.InputText(
+                    default_text=first_use_model.database_table_name, key='table_name')],
+                [SimpleGUI.InputText(
+                    default_text=first_use_model.database_fields, key='fields')],
+            ],
+                            border_width=1),
+        ],
+
+        [
+            SimpleGUI.Frame('SuperUser:', [
+                [SimpleGUI.Text('Администратор:')],
+                [SimpleGUI.Text('Пароль:')]
+            ],
+                            border_width=0),
+
+            SimpleGUI.Frame('Учетные данные администратора БД', [
+                [SimpleGUI.InputText(
+                    default_text=first_use_model.superuser_login, key='login')],
+                [SimpleGUI.InputText(
+                    default_text=first_use_model.superuser_password, key='password')],
+            ],
+                            border_width=1),
+        ],
     ]
 
     window = SimpleGUI.Window('TimeManager 5, personalization', layout, icon='ico/main.ico')
@@ -84,9 +124,25 @@ def personalization():
                     config.write(configfile)
 
                 # save and execute preferences, close
+            case 'next':
+                # Закрываем окно
                 window.close()
                 return 'next personalization'
             case 'Skip':
+                # Передаем в точку входа в приложение первого использования данные с окна
+                return 'next personalization', first_use_model
+            case 'advanced-next':
+                # Закрываем окно
+                window.close()
+                # Передаем в точку входа в приложение первого использования данные с окна
+                first_use_model.uname = value['uname']
+                first_use_model.ui_locale = ('Ru', 'En')[locale_state]
+                first_use_model.database_db_name = value['db_name']
+                first_use_model.database_table_name = value['table_name']
+                first_use_model.database_fields = value['fields']
+                first_use_model.superuser_login = value['login']
+                first_use_model.superuser_password = value['password']
+                return 'next personalization', first_use_model
                 # close and execute default preferences
                 window.close()
                 return 'skip personalization'
@@ -99,13 +155,13 @@ def personalization():
                     # Дефолтную выделяем
                     window[value['More themes']].update(disabled=True)
 
-                # Изменение настроек темы
-                config.set('UI', 'theme', value['More themes'])
+                first_use_model.ui_theme = value['more themes']
 
             case 'Locale':
                 window['Locale'].update(image_data=((Ru_locale, En_locale)[locale_state]))
 
-                config.set('UI', 'locale', ('Ru', 'En')[locale_state])
+                # config.set('UI', 'locale', ('Ru', 'En')[locale_state])
+                first_use_model.ui_locale = ('Ru', 'En')[locale_state]
 
             case SimpleGUI.WIN_CLOSED:
                 window.close()
@@ -116,29 +172,38 @@ def personalization():
                 dg_btn.update(disabled=False)
                 default_btn.update(disabled=False)
                 lb5_btn.update(disabled=False)
+
+                first_use_model.ui_theme = 'DarkBlue13'
+
             case 'DarkGrey':
                 # change focus button
                 db13_btn.update(disabled=False)
                 dg_btn.update(disabled=True)
                 default_btn.update(disabled=False)
                 lb5_btn.update(disabled=False)
+
+                first_use_model.ui_theme = 'DarkGrey'
+
             case 'Default1':
                 # change focus button
                 db13_btn.update(disabled=False)
                 dg_btn.update(disabled=False)
                 default_btn.update(disabled=True)
                 lb5_btn.update(disabled=False)
+
+                first_use_model.ui_theme = 'Default1'
+
             case 'LightBlue5':
                 # change focus button
                 db13_btn.update(disabled=False)
                 dg_btn.update(disabled=False)
                 default_btn.update(disabled=False)
                 lb5_btn.update(disabled=True)
+
+                first_use_model.ui_theme = 'LightBlue5'
+
             case _:
                 pass
-        if event.startswith("URL "):
-            url = event.split(' ')[1]
-            webbrowser.open(url)
 
 
 # Приветствие
@@ -195,7 +260,7 @@ def main():
         if 'next' == button_preset:
             match subchapter:
                 case 'welcome':
-                    next_chapter = personalization()
+                    next_chapter, model = personalization()
                 case 'personalization':
                     next_chapter = confirm()
                 case 'confirm':
@@ -219,7 +284,7 @@ def main():
                 case 'personalization':
                     next_chapter = welcome()
                 case 'confirm':
-                    next_chapter = personalization()
+                    next_chapter, model = personalization()
 
         # Отработка кнопки ПРОПУСТИТЬ, ПОСТАВИТЬ ПО УМОЛЧ.
         if 'skip' == button_preset:
